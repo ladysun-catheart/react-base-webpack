70959f26952cadbbcd726f9dbc4f6d0677243205
Gema Moreno Estevez <ladysun.freedom@gmail.com>
2020-01-06 02:32:52 +0100

    [develop] Improve minimal webpack config

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,6 +1,10 @@
 module.exports = {
+    mode: 'development',
     entry: './src/main.js',
     output: {
         filename: 'bundle.js' 
+    },
+    devServer: {
+        contentBase: './dist'
     }
 }
